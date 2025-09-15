32ace667549220f2a01a66622450f665d07d0db7
bashirafarhin <farhinbashira@gmail.com>
2025-09-15 17:20:41 +0530

    Add reset password  page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ForgotPassword from './newComponents/loginSection/ForgotPassword'
+import ResetLink from './newComponents/loginSection/ResetLink'
 import SignIn from './newComponents/loginSection/SignIn'
 
 const App = () => {
   return (
     <div>
-     <SignIn/>
-     <ForgotPassword />
+     {/* <SignIn/> */}
+     {/* <ForgotPassword /> */}
+     <ResetLink />
     </div>
   )
 }
