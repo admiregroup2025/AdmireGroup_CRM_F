f35bd2bbd4490b18a06f0d65d9dd0fd57f041db1
Poddar54321 <kumarkunal7827@gmail.com>
2025-09-15 14:01:52 +0530

    removed unwanted section of code

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,11 @@
-import React, { lazy , Suspense, useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { CSpinner, useColorModes } from '@coreui/react';
-import './index.css';
-
-// Lazy load components
-const DefaultLayout = lazy(() => import('./layout/DefaultLayout.jsx'));
-const HrLayout = lazy(() => import('./layout/HrLayout.jsx'));
-const AdminLayout = lazy(() => import('./layout/AdminLayout.jsx'));
-const MgrLayout = lazy(() => import('./layout/MgrLayout.jsx'));
-const CompanyPage = lazy(() => import('./views/CompanyPage.jsx'));
-const LoginPage = lazy(() => import('./loginPage/Loginpage.jsx'));
-const ProtectedRoute = lazy(() => import('./components/ProtectedRoute.jsx'));
+import SignIn from './newComponents/loginSection/SignIn'
 
 const App = () => {
-  const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme');
-  const [storedTheme, setStoredTheme] = useState('light');
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.href.split('?')[1]);
-    const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0];
-    
-    if (theme) {
-      setColorMode(theme);
-      setStoredTheme(theme);
-      return;
-    }
-
-    if (!isColorModeSet()) {
-      setColorMode(storedTheme);
-    }
-  }, [storedTheme, isColorModeSet, setColorMode]);
-
   return (
-    <Router>
-      <Suspense fallback={<div className="pt-3 text-center"><CSpinner color="primary" /></div>}>
-        <div className="app-container">
-          <Routes>
-            {/* Public Routes */}
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/" element={<CompanyPage />} />
+    <div>
+     <SignIn/>
+    </div>
+  )
+}
 
-            {/* Protected Routes */}
-            <Route path="/hr" element={
-              // <ProtectedRoute>
-              //   <HrLayout />
-              // </ProtectedRoute>
-                <HrLayout />
-
-            } />
-
-            <Route path="/admin/" element={
-              <ProtectedRoute>
-                <AdminLayout />
-              </ProtectedRoute>
-            } />
-
-            <Route path="/manager/*" element={
-              // <ProtectedRoute>
-                // <MgrLayout />
-              // </ProtectedRoute>
-                <MgrLayout />
-
-            } />
-
-            <Route path="/employee/*" element={
-              // <ProtectedRoute>
-              //   <DefaultLayout />
-              // </ProtectedRoute>
-
-                <DefaultLayout />
-
-            } />
-          </Routes>
-        </div>
-      </Suspense>
-    </Router>
-  );
-};
-
-export default App;
+export default App
--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,12 @@ import { Provider } from "react-redux";
 import "@coreui/coreui/dist/css/coreui.min.css";
 import "./index.css";
 import App from "./App.jsx";
-import store from "./store"; // import your Redux store
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+]);
 
-createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);
