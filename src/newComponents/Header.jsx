8d98a9bfe316d2bbeb35b6786e8f1aa7156b62dc
Poddar54321 <kumarkunal7827@gmail.com>
2025-09-16 12:30:17 +0530

    worked on header

--- src/newComponents/Header.jsx
+++ src/newComponents/Header.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { FiChevronDown, FiLogOut } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+    const navigate = useNavigate();
     return (
-        <header className="flex h-16 w-[82vw] items-center justify-between bg-white px-6 shadow-sm">
+        <header className="flex h-[10vh] w-[82vw] items-center justify-between bg-white px-6 shadow-md">
             <div>
-                <h1 className="text-xl font-semibold text-gray-800">Dashboard Overview</h1>
+                <h1 className="text-[28px] font-semibold text-gray-500">Dashboard Overview</h1>
             </div>
 
             <div className="flex items-center gap-6">
                 <div className="flex cursor-pointer items-center gap-1 rounded-md border border-gray-300 px-3 py-2 transition-shadow duration-200 hover:shadow-md">
                     <span className="text-gray-600">Tech Corp</span>
-                    <FiChevronDown className="text-gray-600" />
+                    {/* <FiChevronDown className="text-gray-600" /> */}
                 </div>
 
-                <div className="flex cursor-pointer items-center gap-3 rounded-md p-2 transition-colors duration-200 hover:bg-gray-100">
-                    <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-800 font-bold text-white">JD</div>
+                <div className="flex cursor-pointer items-center gap-3 rounded-md p-2 transition-colors">
+                    <div className="flex size-[45px] items-center justify-center rounded-full bg-gray-200 text-[20px] font-semibold text-black">
+                        JD
+                    </div>
                     <div className="flex flex-col">
-                        <span className="text-sm font-medium text-gray-700">John Doe</span>
+                        <span className="text-[18px] font-medium text-gray-500">John Doe</span>
                     </div>
                 </div>
 
@@ -25,6 +29,7 @@ const Header = () => {
                     <FiLogOut
                         size={20}
                         className="text-gray-600"
+                        onClick={() => navigate("/")}
                     />
                 </button>
             </div>
