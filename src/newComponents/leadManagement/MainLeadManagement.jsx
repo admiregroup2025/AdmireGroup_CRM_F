7d6a8ba35b086ac93cf3550d6f5a224c9a8dce5a
Poddar54321 <kumarkunal7827@gmail.com>
2025-09-17 00:35:06 +0530

    merged code from barishta

--- src/newComponents/leadManagement/MainLeadManagement.jsx
+++ src/newComponents/leadManagement/MainLeadManagement.jsx
@@ -1,5 +1,5 @@
 import { cardData } from "./data.jsx";
-import Card from "../UserManagement/Card.jsx";
+import MyCard from "../UserManagement/MyCards.jsx";
 import SearchLead from "./SearchLead.jsx";
 import SearchStatus from "./SearchStatus.jsx";
 import AddLead from "./AddLead.jsx";
@@ -7,11 +7,11 @@ import LeadTable from "./LeadTable.jsx";
 
 const MainLeadManagement = () => {
     return (
-        <div className="p-8 bg-[#f8f9fa]">
+        <div className="p-8 max-h-[85vh] overflow-y-auto bg-[#f8f9fa]">
             <div className="flex gap-4">
                 {cardData.map((card, ind) => {
                     return (
-                        <Card
+                        <MyCard
                             title={card.title}
                             value={card.value}
                             icon={card.icon}
