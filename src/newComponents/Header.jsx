e1ba4a5cb51c5b1eff7a386adda8160ea5c67baf
Poddar54321 <kumarkunal7827@gmail.com>
2025-09-16 13:55:38 +0530

    worked on header

--- src/newComponents/Header.jsx
+++ src/newComponents/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FiChevronDown, FiLogOut } from "react-icons/fi";
+import { FiChevronDown, FiLogOut, FiBriefcase } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
@@ -11,9 +11,10 @@ const Header = () => {
             </div>
 
             <div className="flex items-center gap-6">
-                <div className="flex cursor-pointer items-center gap-1 rounded-md border border-gray-300 px-3 py-2 transition-shadow duration-200 hover:shadow-md">
+                <div className="flex cursor-pointer items-center gap-1 rounded-[12px] bg-gray-100 px-3 py-2 transition-shadow duration-200 hover:shadow-md">
+                    <FiBriefcase className="text-gray-600" />
                     <span className="text-gray-600">Tech Corp</span>
-                    {/* <FiChevronDown className="text-gray-600" /> */}
+                    <FiChevronDown className="text-gray-600" />
                 </div>
 
                 <div className="flex cursor-pointer items-center gap-3 rounded-md p-2 transition-colors">
@@ -25,11 +26,13 @@ const Header = () => {
                     </div>
                 </div>
 
-                <button className="rounded-md p-2 transition-colors duration-200 hover:bg-gray-100">
+                <button
+                    onClick={() => navigate("/")}
+                    className="rounded-md p-2 transition-colors duration-200 hover:bg-gray-100"
+                >
                     <FiLogOut
                         size={20}
                         className="text-gray-600"
-                        onClick={() => navigate("/")}
                     />
                 </button>
             </div>
